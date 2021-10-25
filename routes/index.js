09acacb498ca294b5b52e5bc635ef372e2df11c9
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-24 19:32:48 -0500

    added api routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
-const notesRouter = require('./notes');
+const notesRouter = require("./notes.js");
 
 const app = express();
 
-app.use('/notes', notesRouter);
+app.use("/notes", notesRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const path = require("path");
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require("./helpers/fsUtils");
+const api = require("./routes/index.js");
 
 const PORT = process.env.port || 3001;
 
@@ -13,6 +9,7 @@ const app = express();
 //middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 
 app.use(express.static("public"));
 
