f4702a80609c8942c29f5c2b19f94ae63deb4b9f
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-24 20:02:02 -0500

    attempting to debug for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -15,11 +15,11 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(_dirname, "/public/notes.html"))
+  res.sendFile(path.join(_dirname, "./public/notes.html"))
 );
 
 app.listen(PORT, () =>
